a02fbe55ce707ebd4c039fa3205593384b017e63
Peter Pan <peter@never.lan>
2025-03-19 21:09:06 -0400

    run configuration added

--- platforms/bukkit/build.gradle.kts
+++ platforms/bukkit/build.gradle.kts
@@ -1,3 +1,5 @@
+import xyz.jpenilla.runpaper.task.RunServer
+
 plugins {
     id("xyz.jpenilla.run-paper") version Versions.Bukkit.runPaper
 }
@@ -21,13 +23,35 @@ tasks {
         exclude("com/google/j2objc/**")
         exclude("javax/**")
     }
-
-    runServer {
-        minecraftVersion(Versions.Bukkit.minecraft)
-        dependsOn(shadowJar)
-        pluginJars(shadowJar.get().archiveFile)
-    }
 }
 
-
 addonDir(project.file("./run/plugins/Terra/addons"), tasks.named("runServer").get())
+
+val prodPlugins = runPaper.downloadPluginsSpec {
+    modrinth("terra", "6.6.1-BETA-bukkit")
+    modrinth("flycraft", "1.4.2")
+    modrinth("multiverse-core", "4.3.15-pre.2")
+    modrinth("multiverse-inventories", "4.2.7-pre")
+    modrinth("multiverse-portals", "4.3.0-pre")
+    modrinth("multiverse-netherportals", "4.3.0-pre.3")
+}
+
+val testPlugins = runPaper.downloadPluginsSpec {
+    from(prodPlugins) // Copy everything from prod
+    github("Ifiht", "AutoStop", "v1.2.0", "AutoStop-1.2.0.jar")
+}
+
+// Test PaperMC run & immediately shut down, for github actions
+tasks.register<RunServer>("runServerTest") {
+    //dependsOn(tasks.shadowJar)
+    minecraftVersion("1.21.4")
+    downloadPlugins.from(testPlugins)
+    pluginJars.from(tasks.shadowJar)
+}
+// Start a local PaperMC test server for login & manual testing
+tasks.register<RunServer>("runServerInteractive") {
+    //dependsOn(tasks.shadowJar)
+    minecraftVersion("1.21.4")
+    downloadPlugins.from(prodPlugins)
+    pluginJars.from(tasks.shadowJar)
+}
